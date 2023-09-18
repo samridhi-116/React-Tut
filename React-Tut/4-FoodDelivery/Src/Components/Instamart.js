5a6c32be963327abe81f0d3e4a7a09fb4c873647
samridhi-116 <samridhi1311@chitkara.edu.in>
2023-09-18 13:46:00 +0530

    ..

--- React-Tut/4-FoodDelivery/Src/Components/Instamart.js
+++ React-Tut/4-FoodDelivery/Src/Components/Instamart.js
@@ -23,11 +23,8 @@ const Instamart = () =>{
     const [decDisplay, setDecDisplay] = useState(false);
     return(
         <>
-            {/* Creating a functionality for FAQ's */}
             <h1>Welsome to Swiggy's Instamart</h1>
-            <h2>
-            Dairy, bread & eggs delivered in minutes, from the house of Swiggy.
-            </h2>
+            <h2>Dairy, bread & eggs delivered in minutes, from the house of Swiggy.</h2>
             <br/>
             <br/>
             <h2>FAQ's</h2>
@@ -38,25 +35,25 @@ const Instamart = () =>{
             <Section
                 title={"About Instamart"}
                 description={"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC"}
-                isVisible={decDisplay === "about"}
                 setIsVisible={()=>{setDecDisplay("about")}}
+                isVisible={decDisplay === "about"}
             />
             <Section
                 title={"Career Instamart"}
                 description={"Making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage"}
-                isVisible={decDisplay === "career"}
                 setIsVisible={()=>{setDecDisplay("career")}}
+                isVisible={decDisplay === "career"}
             />
             <Section
                 title={"Support Instamart"}
                 description={"Going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1"}
-                isVisible={decDisplay==="support"}
                 setIsVisible={()=>{setDecDisplay("support")}}
+                isVisible={decDisplay==="support"}
             />
             <Section
                 title={"Contact Instamart"}
                 description={"10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32."}
-                isVisible={decDisplay ==="contact"}
+                isVisible={decDisplay === "contact"}
                 setIsVisible={()=>{setDecDisplay("contact")}}
             />
         </>
